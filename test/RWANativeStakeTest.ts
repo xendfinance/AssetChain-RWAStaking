16d0f6952bd9d09cb4aa0c86327a27b8572c1f8c
rony4d <rony4d@gmail.com>
2024-06-02 22:44:17 +0100

    Stake testcases

--- test/RWANativeStakeTest.ts
+++ test/RWANativeStakeTest.ts
@@ -31,27 +31,28 @@ describe("RWA Native Stake Contract", function(){
     describe("Deployment", function(){
 
       it("Should deploy contract with correct initial parameters", async function () {
+
          
-          // We use deployContractFixture to setup our environment, and then assert that
-          // things went well
+        // We use deployContractFixture to setup our environment, and then assert that
+        // things went well
 
-          const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-          const ONE_WEEK_IN_SECS = 7 * 24 * 60 * 60;
-          const ONE_DAY_IN_SECS = 24 * 60 * 60;
+        const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+        const ONE_WEEK_IN_SECS = 7 * 24 * 60 * 60;
+        const ONE_DAY_IN_SECS = 24 * 60 * 60;
           
 
-          const { stakeContract, owner } = await loadFixture(deployContractFixture);
+        const { stakeContract, owner } = await loadFixture(deployContractFixture);
 
           
-          expect(await stakeContract.treasury()).to.equal(0);
-          expect(await stakeContract.totalStaked()).to.equal(0);
-          expect(await stakeContract.totalStakers()).to.equal(0);
+        expect(await stakeContract.treasury()).to.equal(0);
+        expect(await stakeContract.totalStaked()).to.equal(0);
+        expect(await stakeContract.totalStakers()).to.equal(0);
 
-          expect(await stakeContract.lockTime()).to.equal(ONE_WEEK_IN_SECS);
-          expect(await stakeContract.actionLimit()).to.equal(ONE_DAY_IN_SECS);
+        expect(await stakeContract.lockTime()).to.equal(ONE_WEEK_IN_SECS);
+        expect(await stakeContract.actionLimit()).to.equal(ONE_DAY_IN_SECS);
 
-          expect(await stakeContract.government()).to.equal(owner.address);
-          expect(await stakeContract.reductionPercent()).to.equal(3000);
+        expect(await stakeContract.government()).to.equal(owner.address);
+        expect(await stakeContract.reductionPercent()).to.equal(3000);
 
 
 
@@ -60,5 +61,69 @@ describe("RWA Native Stake Contract", function(){
 
 
   });
+
+  describe("Stake Operation", function(){
+
+    it("Should allow owner to stake with flexible stake : 0 LOCK_PERIOD", async function () {
+       
+      const { stakeContract, owner, addr1 } = await loadFixture(deployContractFixture);
+      const amountToStake = hre.ethers.parseEther("1"); // Staking 1 RWA
+
+      await stakeContract.stake(0, { value: amountToStake });
+      expect(await stakeContract.deposits(1)).to.equal(amountToStake);
+      expect(await stakeContract.getStakingIds(owner.address)).to.have.lengthOf(1);
+
+
+
+    });
+
+    it("Should allow owner user stake with flexible stake ", async function () {
+      
+      const { stakeContract, owner, addr1 } = await loadFixture(deployContractFixture);
+      const amountToStake = hre.ethers.parseEther("1"); // Staking 1 RWA
+      await stakeContract.connect(addr1).stake(0, { value: amountToStake });
+      expect(await stakeContract.getStakingIds(addr1.address)).to.have.lengthOf(1);
+
+
+    });
+
+
+    it("Should allow another user stake with flexible stake ", async function () {
+      
+      const { stakeContract, owner, addr1 } = await loadFixture(deployContractFixture);
+      const amountToStake = hre.ethers.parseEther("1"); // Staking 1 RWA
+      await stakeContract.connect(addr1).stake(0, { value: amountToStake });
+      expect(await stakeContract.getStakingIds(addr1.address)).to.have.lengthOf(1);
+
+
+    });
+
+    it("Should allow another user do a fixed stake : 3 Months LOCK_PERIOD ", async function () {
+      
+      const ONE_MONTH = 30 * 24 * 60 * 60;
+
+      const { stakeContract, owner, addr1 } = await loadFixture(deployContractFixture);
+      const amountToStake = hre.ethers.parseEther("10"); // Staking 10 RWA
+      await stakeContract.connect(addr1).stake(0, { value: amountToStake });
+      expect(await stakeContract.getStakingIds(addr1.address)).to.have.lengthOf(1);
+
+      // Advance the blockchain's timestamp by 3 months
+
+      var currentBlock = await time.latestBlock();
+      console.log("Current Block Now: %s", currentBlock);
+      await time.increase(ONE_MONTH * 3);
+      currentBlock = await time.latestBlock();
+      console.log("Current Block Future: %s", currentBlock);
+
+      // Now you can test the unstake function
+      await stakeContract.connect(addr1).unstake();
+      expect(await stakeContract.getStakingIds(addr1.address)).to.have.lengthOf(0);
+    });
+
+    it("Should not allow user to stake with invalid lock period ", async function () {
+       
+      
+    });
+});
 
 });
