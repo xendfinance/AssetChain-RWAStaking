0d27b643722542b743ce117e1b2985302bb1bfa2
rony4d <rony4d@gmail.com>
2024-08-07 12:52:39 +0100

    MI - 03 Useless imports

--- contracts/RWANativeStake.sol
+++ contracts/RWANativeStake.sol
@@ -5,8 +5,6 @@ pragma solidity 0.8.20;
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /**
   @title Staking implementation 
